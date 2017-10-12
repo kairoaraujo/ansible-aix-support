6af52561a488cb12e975bf7c9000220a23795b74
Kairo Araujo <kairo@kairo.eti.br>
2017-10-12 19:14:44 +0000

    Improved install script and added new installp
    Added installp according PR 30238 update
    Install script was improved to better management

--- install-ansible-aix-support.sh
+++ install-ansible-aix-support.sh
@@ -34,7 +34,6 @@ if [ "${ansible_version}" != "2.4" ]; then
 fi
 echo "[INFO] Version compatible."
 
-echo "[INFO] Starting copies."
 if [ ! -d lib/ ]; then
    echo "[ERROR] Directory lib not found. Are you in the Ansible AIX support dir?"
    exit 1
@@ -42,17 +41,27 @@ fi
 
 ansible_lib_path=$(echo ${ansible_data} | awk -F "ansible python module location =" '{ print $2 }' | awk '{ print $1 }')
 for file in $(find lib/ | grep .py); do
-    destination_file_path=$(echo ${file} | awk -F 'ansible' '{ print $2 }')
-    if [ -f ${ansible_lib_path}${destination_file_path} ]; then
+  destination_file_path=$(echo ${file} | awk -F 'ansible' '{ print $2 }')
+
+  if [ -f ${ansible_lib_path}${destination_file_path} ]; then
+      check_diff=$(diff ${file} ${ansible_lib_path}${destination_file_path} > /dev/null 2>&1)
+      if [ $? -ne 0 ]; then
+        if [ ! -d backup ]; then
+          echo "[INFO] Creating backup directory"
+          mkdir backup
+        fi
         echo "[INFO] Saving backup file ${ansible_lib_path}${destination_file_path}"
-        cp ${ansible_lib_path}${destination_file_path} ${ansible_lib_path}${destination_file_path}_$(date "+%m%d%H%M%Y.%S")
-    fi
-    echo "[INFO] Adding ${ansible_lib_path}${destination_file_path}"
-    cp ${file} ${ansible_lib_path}${destination_file_path}
+        cp ${ansible_lib_path}${destination_file_path} backup/$(echo $file | awk -F "/" '{ print $NF }')_$(date "+%m%d%H%M%Y.%S")
+        echo "[INFO] Adding ${ansible_lib_path}${destination_file_path}"
+        cp ${file} ${ansible_lib_path}${destination_file_path}
+      else
+        echo "[INFO] No update required: ${ansible_lib_path}${destination_file_path}"
+      fi
+  else
+      echo "[INFO] Adding ${ansible_lib_path}${destination_file_path}"
+      cp ${file} ${ansible_lib_path}${destination_file_path}
+  fi 
 done
 
 echo "[INFO] Finished."
 echo
-    
-
-
